6b1597dbfc8a6ac8cb3fccf27aab5c7ae4ceeb92
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-03-06 16:57:53 +0530

    a1 init

---
+++ a1/lib/utils.py
@@ -0,0 +1,4 @@
+import os 
+
+def getFileNames(path_dataset):
+    return os.listdir(path_dataset)
---
+++ a1/main.py
@@ -0,0 +1,8 @@
+import os 
+
+from lib.constants import (
+    PATH_DATASET
+)
+
+if __name__=="__main__":
+    print(os.listdir(PATH_DATASET))
