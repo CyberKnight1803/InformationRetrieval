5ec2355cc6bdde26910465bb32cc7c676764cce6
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-03-08 00:15:22 +0530

    add inverted_index data structure

--- a1/lib/utils.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-def getFileNames(path_dataset):
-    return os.listdir(path_dataset)
---
+++ a1/utils/posting.py
@@ -0,0 +1,16 @@
+class Posting:
+    """
+        Posting list for each token 
+    """
+    
+    def __init__(self):
+        self.freq = 1
+        self.list= []
+
+    def __str__(self) -> str:
+        return "Posting list for each token"
+
+
+
+# Posting Utils  
+
