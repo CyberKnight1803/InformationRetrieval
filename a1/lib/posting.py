e346372bc8b2a8af1c89d20abe6436f271911410
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-03-17 11:32:24 +0530

    add zone index

--- a1/lib/posting.py
+++ a1/lib/posting.py
@@ -3,9 +3,14 @@ class Posting:
         Posting list for each token 
     """
     
-    def __init__(self):
+    def __init__(self, zones = ["title", "meta", "characters", "body"]):
         self.freq = 1
-        self.list= []
+        self.list = {
+            "title": [], 
+            "meta": [], 
+            "characters": [], 
+            "body": []
+        }
 
     def __str__(self) -> str:
         return "Posting list for each token"
---
+++ a1/lib/zone_index.py
@@ -0,0 +1,64 @@
+from posting import Posting
+
+# INVERTED INDEX VISUALIZATION
+#
+# zone_index = {
+#     token: {
+#         "freq": 1
+#         "zone-1": [docID_1, docID_2, ....],
+#         "zone-2": [docID_1, docID_2, ....]
+#         ...
+#     }
+# }
+
+def createZoneIndex(docs):
+    """
+        Constructs a zone indexing for the preprocessed docs
+    """
+    
+    zone_index = {}
+
+    for docID, docContent in docs.items():
+        for zone in docContent.keys():
+            for token in docContent[zone]:
+                if token in zone_index.keys():
+                    zone_index[token].freq += 1
+                    if zone_index[token].list[zone][-1] != docID:
+                        zone_index[token].list[zone].append(docID)
+                
+                else:
+                    zone_index.update({token: Posting()})
+                    zone_index[token].list[zone].append(docID)
+
+    return zone_index
+
+def printZoneIndex(zone_index):
+    """
+        Prints zone index on console for visualization!
+    """
+    print("ZONE-INDEX")
+
+    for token, posting in zone_index.items():
+        for zone in posting.list.keys():
+            print("{token}.{zone:<20}{list:<10}".format(token=token, zone=zone, list=str(posting.list[zone])))
+        print("\n")
+
+def dummy_index():
+    tokens = ["token_1", "token_2", "token_3"]
+
+    zone_index = {}
+
+    for token in tokens:
+        a_post = Posting()
+        a_post.list["title"] = [1, 7, 12, 13, 25]
+        a_post.list["meta"] = [3, 10]
+        a_post.list["characters"] = []
+        a_post.list["body"] = [10, 14, 21]
+
+        zone_index.update({token: a_post})
+    
+    return zone_index
+
+if __name__=="__main__":
+    printZoneIndex(dummy_index())
+
